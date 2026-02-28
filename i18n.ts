73913ed3f946eb8207be55690805407274243892
KTCrisis <ktcrisis@gmail.com>
2026-02-28 10:26:13 +0100

    fix: i18n ?

--- i18n.ts
+++ i18n.ts
@@ -1,11 +1,11 @@
-import { getRequestConfig } from 'next-intl/server'
+import { getRequestConfig } from 'next-intl/server';
 
 export default getRequestConfig(async ({ requestLocale }) => {
-  const locale = (await requestLocale) ?? 'fr'
+  const locale = (await requestLocale) ?? 'fr';
 
-  const messages = locale === 'en'
-    ? (await import('./messages/en.json')).default
-    : (await import('./messages/fr.json')).default
-
-  return { locale, messages }
-})
+  return {
+    locale,
+    // Cette syntaxe est mieux interprétée par les bundlers de Workers
+    messages: (await import(`./messages/${locale}.json`)).default
+  };
+});
